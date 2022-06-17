c18cf30432331ebc2beaa964f2bf3836ab702561
naimur202163 <naimur202163@gmail.com>
2022-06-17 19:27:15 +0600

    getALlproducts route added

--- backend/controllers/productCotroller.js
+++ backend/controllers/productCotroller.js
@@ -1,16 +1,20 @@
-const Product =require('../models/productModel')
+const Product = require("../models/productModel");
 
-
-// Create Products
+// Create Product --Admin
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
-
   res.status(201).json({
     success: true,
     product,
   });
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+// Get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
