6bcc1c560781fec876cac5665ffd20cdb026a678
naimur <naimur202163@gmail.com>
2022-07-06 04:30:06 +0600

    password hasing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-
+const bcrypt = require("bcryptjs");
 // User Model for authentication for mongodb
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,4 +38,15 @@ const userSchema = new mongoose.Schema({
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 });
+
+// Hashing the password
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
 module.exports = mongoose.model("User", userSchema);
