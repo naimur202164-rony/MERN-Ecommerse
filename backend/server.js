e18d0b74317e869e4decb57b80ea083e43322c35
naimur <naimur202163@gmail.com>
2022-07-05 14:37:47 +0600

    error hander added

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,26 @@ const app=require('./app');
 // Data Base Connection
 const connectDatabase=require("./config/database");
 
+// Handling Uncaught Exception
+process.on("uncaughtException",(err)=>{
+console.log(`Error:${err.message }`);
+console.log(`Shuting down the server due to Uncaught Exception`);
+process.exit(1);
+})
 
 connectDatabase()
 
-app.listen(5000,()=>{
+const server= app.listen(5000,()=>{
     console.log(`the server is running on port 5000`)
+})
+
+
+
+// Unhandled Promis Rejections
+process.on("unhandledRejection",err=>{
+    console.log(`Error:${err.message} `);
+    console.log(`Shutting down the server due to unhandled Promise Rejections`);
+    server.close(()=>{
+        process.exit(1)
+    })
 })
