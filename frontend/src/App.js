df6a8246bdf2bdc55245c8f8c5ad2cb897b8bc82
naimur <naimur202163@gmail.com>
2022-07-13 14:10:42 +0600

    header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Header from "./components/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
 export default function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Header />
+    </Router>
+  );
 }
