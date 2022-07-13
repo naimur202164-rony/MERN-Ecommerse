0d44ea7dd76165b1f65223338b0096a8aca9ecf3
naimur <naimur202163@gmail.com>
2022-07-13 19:22:56 +0600

    Router added:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from "./components/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import Footer from "./components/layout/Footer/Footer.js";
+import Home from "./components/Home/Home.js";
 export default function App() {
   React.useEffect(() => {
     WebFont.load({
@@ -13,8 +14,11 @@ export default function App() {
   }, []);
   return (
     <Router>
-    <Header/>
-    <Footer/>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
