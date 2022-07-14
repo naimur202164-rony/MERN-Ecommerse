baf7a98d8429ac258286b57b183ca0f4d0af7577
naimur <naimur202163@gmail.com>
2022-07-14 06:46:14 +0600

    banner added

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Button } from "@material-ui/core";
+import React, { Fragment } from "react";
+import { CgMouse } from "react-icons/all";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="banner">
+        <p>Wellcome to Ecommerse</p>
+        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>
+            scroll
+            <CgMouse />
+          </button>
+        </a>
+      </div>
 
-export default Home
+      <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container"></div>
+    </>
+  );
+};
+
+export default Home;
