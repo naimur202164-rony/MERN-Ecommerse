70b36c6fafaf5ddd51698cf1ad37bb192cf6d8cc
naimur <naimur202163@gmail.com>
2022-07-14 08:30:09 +0600

    constants are added

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+let initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
