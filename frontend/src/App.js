5f38d0ea939a1c7699f2f2e465a8a16168774a35
naimur <naimur202163@gmail.com>
2022-07-15 04:37:32 +0600

    loader added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WebFont from "webfontloader";
 import Footer from "./components/layout/Footer/Footer.js";
 import Home from "./components/Home/Home.js";
+import Loader from "./components/layout/Loader/Loader.js";
 export default function App() {
   React.useEffect(() => {
     WebFont.load({
@@ -17,6 +18,7 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/sad" component={Loader} />
       </Switch>
       <Footer />
     </Router>
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -5,39 +5,46 @@ import MetaData from "../layout/MetaData.js";
 import "./Home.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getProduct } from "../../actions/productAction.js";
-
+import Loader from "../layout/Loader/Loader.js";
 
 const Home = () => {
-const dispatch = useDispatch();
-const {loding,error,products,productCount}=useSelector((state)=>state.products);
+  const dispatch = useDispatch();
+  const { loading, error, products, productCount } = useSelector(
+    (state) => state.products
+  );
 
-console.log(products)
+  console.log(products);
   useEffect(() => {
     dispatch(getProduct());
   }, [dispatch]);
   return (
-    <>
-      <MetaData title="ECCOMERSE" />
-      <div className="banner">
-        <p>Wellcome to Ecommerse</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#container">
-          <button>
-            scroll
-            <CgMouse />
-          </button>
-        </a>
-      </div>
+    <Fragment>
+      {loading ? (
+        <Loader/>
+      ) : (
+        <>
+          <MetaData title="ECCOMERSE" />
+          <div className="banner">
+            <p>Wellcome to Ecommerse</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                scroll
+                <CgMouse />
+              </button>
+            </a>
+          </div>
 
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        {
-          products && products.map(product=> (
-             <Product key={product._id} product={product}/>
-          ))
-        }
-      </div>
-    </>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))}
+          </div>
+        </>
+      )}
+    </Fragment>
   );
 };
 
