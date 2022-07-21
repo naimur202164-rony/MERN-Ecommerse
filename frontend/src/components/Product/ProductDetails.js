a1cc4a160971661116dbaa1a969e4158a9db36c4
naimur <naimur202163@gmail.com>
2022-07-21 08:22:11 +0600

    loader is added in product details page

--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -3,7 +3,8 @@ import React, { Fragment, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
-import ReviewCard from './ReviewCard.js'
+import ReviewCard from './ReviewCard'
+import Loader from '../layout/Loader/Loader.js'
 import "./ProductDetails.css";
 const ProductDetails = ({ match }) => {
   const dispatch = useDispatch();
@@ -22,77 +23,83 @@ const ProductDetails = ({ match }) => {
     precision: 0.5,
   };
   return (
-    <Fragment>
-      <div className="ProductDetails">
-        <div>
-          <Carousel>
-            {product.images &&
-              product.images.map((item, i) => (
-                <img
-                  className="CarouselImage"
-                  key={i}
-                  src={item.url}
-                  alt={`${i} Slide`}
-                />
-              ))}
-          </Carousel>
-        </div>
-        {/* Secound div */}
-        <div>
-          <div className="detailsBlock-1">
-            <h2>{product.name}</h2>
-            <p>Product # {product._id}</p>
+   <Fragment>
+    {
+      loading?<Loader/> :(
+        <Fragment>
+        <div className="ProductDetails">
+          <div>
+            <Carousel>
+              {product.images &&
+                product.images.map((item, i) => (
+                  <img
+                    className="CarouselImage"
+                    key={i}
+                    src={item.url}
+                    alt={`${i} Slide`}
+                  />
+                ))}
+            </Carousel>
           </div>
-          <div className="detailsBlock-2">
-            <Rating {...options} />
-            <span className="detailsBlock-2-span">
-              {" "}
-              ({product.numOfReviews} Reviews)
-            </span>
-          </div>
-          <div className="detailsBlock-3">
-            <h1>{`₹${product.price}`}</h1>
-            <div className="detailsBlock-3-1">
-              <div className="detailsBlock-3-1-1">
-                <button>-</button>
-                <input readOnly type="number" />
-                <button>+</button>
-              </div>
-              <button disabled={product.Stock < 1 ? true : false}>
-                Add to Cart
-              </button>
+          {/* Secound div */}
+          <div>
+            <div className="detailsBlock-1">
+              <h2>{product.name}</h2>
+              <p>Product # {product._id}</p>
             </div>
-
-            <p>
-              Status:
-              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                {product.Stock < 1 ? "OutOfStock" : "InStock"}
-              </b>
-            </p>
+            <div className="detailsBlock-2">
+              <Rating {...options} />
+              <span className="detailsBlock-2-span">
+                {" "}
+                ({product.numOfReviews} Reviews)
+              </span>
+            </div>
+            <div className="detailsBlock-3">
+              <h1>{`₹${product.price}`}</h1>
+              <div className="detailsBlock-3-1">
+                <div className="detailsBlock-3-1-1">
+                  <button>-</button>
+                  <input readOnly type="number" />
+                  <button>+</button>
+                </div>
+                <button disabled={product.Stock < 1 ? true : false}>
+                  Add to Cart
+                </button>
+              </div>
+  
+              <p>
+                Status:
+                <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                  {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                </b>
+              </p>
+            </div>
+  
+            <div className="detailsBlock-4">
+              Description : <p>{product.description}</p>
+            </div>
+            <button className="submitReview">Submit Review</button>
           </div>
-
-          <div className="detailsBlock-4">
-            Description : <p>{product.description}</p>
+        </div>
+  
+        <h3 className="reviewHeading">Reviews</h3>
+  
+        {product.reviews && product.reviews[0] ? (
+          <div className="reviews">
+            {product.reviews &&
+              product.reviews.map((review) => (
+                <ReviewCard review={review}></ReviewCard>
+              ))}
           </div>
-          <button className="submitReview">Submit Review</button>
-        </div>
-      </div>
-
-      <h3 className="reviewHeading">Reviews</h3>
-
-      {product.reviews && product.reviews[0] ? (
-        <div className="reviews">
-          {product.reviews &&
-            product.reviews.map((review) => (
-              <ReviewCard review={review}></ReviewCard>
-            ))}
-        </div>
-      ) : (
-        <p className="noReviews">No Reviews Yet</p>
-      )}
-
-      {/*  */}
-    </Fragment>
+        ) : (
+          <p className="noReviews">No Reviews Yet</p>
+        )}
+  
+        {/*  */}
+      </Fragment>
+      )
+    }
+   </Fragment>
   );
 };
 
