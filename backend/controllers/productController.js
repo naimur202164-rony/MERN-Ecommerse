cc95c66a9c8e7f8d993dbe1b29e8703e66936f7b
naimur <naimur202163@gmail.com>
2022-10-21 16:35:22 +0600

    router added

---
+++ backend/controllers/productController.js
@@ -0,0 +1,3 @@
+exports.getAllProducts=(req,res)=>{
+    res.status(200).json({message:"The Route is Working "})
+}
--- backend/routes/productController.js
+++
@@ -1,3 +0,0 @@
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:"The Route is Working "})
-}
