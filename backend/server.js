618c97e9e52d8f68e54c7272d043451021a8e66e
naimur <naimur202163@gmail.com>
2022-10-21 18:04:44 +0600

    database connction added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const app=require('./app');
 const dontenv=require('dotenv');
-
 dontenv.config({path:"backend/config/config.env"})
+const  connectDatabase=require('./config/database')
+
+
+// Database Connection
+
+connectDatabase()
 
 
 
+
+// server
 app.listen(process.env.PORT,()=>{
-    console.log(`The server is running on localhost ${process.env.PORT}` )
+    console.log(`Server is Working on on localhost:${process.env.PORT}`)
 })
