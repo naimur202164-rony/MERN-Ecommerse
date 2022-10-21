ac460259437c5b5f39ef6badc380753df6fdfe24
naimur <naimur202163@gmail.com>
2022-10-21 18:23:31 +0600

    productModel created

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,15 +1,71 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter the Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter the Product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter The price"],
+    maxLength: [8, "Price can not exceed  8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type:String,
+        required:[true,"Please Enter Product Category"],
 
-
-const productSchema=new mongoose.Schema({
+  },
+  Stock:{
+    type:Number,
+    required:[true,"Please Enter Product Stock"],
+    maxLength:[4,"stock can not excced "],
+    default:1,
+  },
+  numOfReviews:{
+    type:Number,
+    default:0
+  },
+  reviews:[{
     name:{
         type:String,
-        required:[true,"Please Enter the Name"],
-        trim:true
+        required:true,
+
     },
-    description:{
+    rating:{
+        type:Number,
+        required:true
+    },
+    comment:{
         type:String,
-        required:[true,"Please Enter the Product Description"]
+        required:true
     }
-})
+  }],
+  createAt:{
+    type:Date,
+    default:Date.now
+  }
+});
+
+
+module.exports=mongoose.model("Product",productSchema)
