ae2d9a4e70c9daa210488c05514a60eb5ae503b2
naimur <naimur202163@gmail.com>
2022-10-21 20:17:33 +0600

    errorMiddleware added

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,19 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
 
+const errorMiddleware = require("./middleware/error");
 
 // config
 
-
-app.use(express.json())
+app.use(express.json());
 
 // Routes Imports
 
+const product = require("./routes/productRoute");
+app.use("/api/v1", product);
 
-const product=require('./routes/productRoute');
-app.use('/api/v1',product);
+// middleware for Errors
 
+app.use(errorMiddleware);
 
-
-
-
-
-
-
-
-
-module.exports=app;
+module.exports = app;
