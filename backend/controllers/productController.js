11bfb5a72e014ad929e8f7504f12bd6b338438d9
naimur <naimur202163@gmail.com>
2022-10-21 18:54:33 +0600

    all products loadded successfully

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,20 @@
 const Product = require("../models/productModel");
 
-// create Product
+// create Product  if admin
 
 exports.createProducts = async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
-    success:true,
-    product
+    success: true,
+    product,
   });
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "The Route is Working " });
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res
+    .status(200)
+    .json({ message: "All Products Loaded ", success: true, products });
 };
