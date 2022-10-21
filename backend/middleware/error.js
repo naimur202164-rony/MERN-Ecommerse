3c612f490dfb10e1bfb3310a1041f6a90eb65163
naimur <naimur202163@gmail.com>
2022-10-21 21:28:21 +0600

    all error handled

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,8 +1,33 @@
 const ErrorHander = require("../utils/errorhander");
 
+
+
+
+
+
+
+
+
+
 module.exports = (err,req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
+
+
+
+
+  // Worng Mongodb Errors
+  if(err.name==="castError"){
+    const message=`Resource  not found  invalid :${err.path}`;
+    err=new ErrorHander(message,400 );
+  }
+
+
+
+
+
+
+
 
     res.status(err.statusCode).json({
         success:false,
