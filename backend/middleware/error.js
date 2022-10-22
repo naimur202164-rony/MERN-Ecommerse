73459fe104d2b68b6e2fb086f733dd1b3591321b
naimur <naimur202163@gmail.com>
2022-10-22 11:27:06 +0600

    working on query

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -13,21 +13,11 @@ module.exports = (err,req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
-
-
-
   // Worng Mongodb Errors
   if(err.name==="castError"){
     const message=`Resource  not found  invalid :${err.path}`;
-    err=new ErrorHander(message,400 );
+    err=new ErrorHander(message,400 );  
   }
-
-
-
-
-
-
-
 
     res.status(err.statusCode).json({
         success:false,
---
+++ backend/utils/apifeatures.js
@@ -0,0 +1,3 @@
+class ApiFeatures{
+    constructor(query)
+}
