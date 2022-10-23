12b0b64648777b872fe14e67ec5efc0c68e24768
naimur <naimur202163@gmail.com>
2022-10-23 18:03:43 +0600

    authintication added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,10 +6,10 @@ const {
   deleteProduct,
   getProductDetails,
 } = require("../controllers/productController");
-const {isAuthenticatedUser}=require('../middleware/auth');
+const {isAuthenticatedUser, authorizeRoles}=require('../middleware/auth');
 const router = express.Router();
 
-router.route("/products").get( getAllProducts);
+router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"), getAllProducts);
 
 router.route("product/new").post(createProduct);
 
