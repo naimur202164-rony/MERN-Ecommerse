b443a4019bef1b138f2d5b34dd91a04d4ab3d57f
naimur <naimur202163@gmail.com>
2022-10-24 14:09:17 +0600

    bugs fixed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,9 +5,6 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
-  createProductReview,
-  getProductReviews,
-  deleteReview,
   getAdminProducts,
 } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
@@ -17,17 +14,13 @@ const router = express.Router();
 router.route("/products").get(getAllProducts);
 
 router
-  .route("/admin/products")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
+  .route("/product/new")
+  .post(isAuthenticatedUser, createProduct);
 
 router
-  .route("/admin/product/new")
-  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
-
-router
-  .route("/admin/product/:id")
-  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+  .route("/product/:id")
+  .put(isAuthenticatedUser,updateProduct)
+  .delete(isAuthenticatedUser,  deleteProduct);
 
 router.route("/product/:id").get(getProductDetails);
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,11 +7,7 @@ const {
   resetPassword,
   getUserDetails,
   updatePassword,
-  updateProfile,
-  getAllUser,
-  getSingleUser,
-  updateUserRole,
-  deleteUser,
+  
 } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
