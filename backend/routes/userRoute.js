374213b2ef38b912c6055326b5f48b45646c15a4
naimur <naimur202163@gmail.com>
2022-10-24 11:47:32 +0600

    reset password token is added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,8 @@ const {
   registerUser,
   loginUser,
   logout,
+  forgotPassword,
+  resetPassword,
  
 } = require("../controllers/userController");
 
@@ -11,5 +13,8 @@ const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route('/logout').post(logout);
+router.route("/password/forget").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
 
 module.exports = router;
