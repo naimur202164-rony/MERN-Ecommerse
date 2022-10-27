2d5717cf39b61d00e3475e4df23c98703357c02f
naimur <naimur202163@gmail.com>
2022-10-27 15:14:41 +0600

    user profile updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -210,3 +210,38 @@ exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
     user,
   });
 });
+
+// update User Profile Role
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+  // We will  addd cloudnary latter
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delet Users ---admin
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  // We will remove cloudinary
+  if (!user) {
+    return next(new ErrorHander(`User Does not exixts with id ${req.params.id}`));
+  }
+
+  await user.remove()
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,6 +10,9 @@ const {
   updateProfile,
   getAllUsers,
   getSingleUser,
+  updateUser,
+  updateUserRole,
+  deleteUser,
 } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
@@ -29,10 +32,13 @@ router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
-router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),getAllUsers);
-router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser);
- 
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
 module.exports = router;
-
-
-
