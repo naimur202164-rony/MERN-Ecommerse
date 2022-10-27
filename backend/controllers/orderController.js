40cf626bf81c8242735e554e2bf0ed2485bafef0
naimur <naimur202163@gmail.com>
2022-10-27 20:27:37 +0600

    ordr controller added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+const ErrorHander = require("../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+const Order = require("../models/orderModel");
+
+exports.newOder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    shipingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shipingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shipingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shipingPrice,
+    totalPrice,
+    paidAt: Date.now,
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
